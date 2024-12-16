862e55f574eb3c04d46b15d37c021cc3b445b39e
AdilxanovAbbaz <adilxanovabbaz1@gmail.com>
2024-12-16 19:10:19 +0500

    Laravel

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\TokenAbilityEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
@@ -9,28 +10,84 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request)
+    /**
+     * Summary of login
+     * @param \App\Http\Requests\Auth\LoginRequest $request
+     * @throws \Illuminate\Auth\AuthenticationException
+     * @throws \Illuminate\Database\Eloquent\ModelNotFoundException
+     * @return mixed|\Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request): JsonResponse
     {
         try {
-             $User = User::where('email', $request->email)->firstOrFail();
+            $user = User::where('email', $request->email)->firstOrFail();
 
-             if (!Hash::check($request->password, $User->password)){
+            if (!Hash::check($request->password, $user->password)) {
                 throw new AuthenticationException();
-             }
-                auth()->login($User);
+            }
 
-                return response()->json([
-                    'token' => $User->createToken('core api')->plainTextToken
-                ]);
+            auth()->login($user);
 
-        } catch (ModelNotFoundException) {
-            throw new ModelNotFoundException("paydalamiwshi tabilmadi");
+            $accessTokenExpiration = now()->addMinutes(config('sanctum.ac_expiration'));
+            $refreshTokenExpiration = now()->addMinutes(config('sanctum.rt_expiration'));
+
+            $accessToken =  auth()->user()->createToken(
+                name: 'access token',
+                abilities: [TokenAbilityEnum::ACCESS_TOKEN->value],
+                expiresAt: $accessTokenExpiration
+            );
+
+            $refreshToken =  auth()->user()->createToken(
+                name: 'refresh token',
+                abilities: [TokenAbilityEnum::ISSUE_ACCESS_TOKEN->value],
+                expiresAt: $refreshTokenExpiration
+            );
+
+            return response()->json([
+                'access_token' => $accessToken->plainTextToken,
+                'refresh_token' => $refreshToken->plainTextToken,
+                'at_expired_at' => $accessTokenExpiration->format('Y-m-d H:i:s'),
+                'rf_expired_at' => $refreshTokenExpiration->format('Y-m-d H:i:s'),
+            ]);
+        } catch (ModelNotFoundException $ex) {
+            throw new ModelNotFoundException("paydalaniwshi tabilmadi");
         }
     }
 
+    /**
+     * Summary of refreshToken
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function refreshToken(): JsonResponse
+    {
+        $accessTokenExpiration = now()->addMinutes(config('sanctum.ac_expiration'));
 
+        $accessToken =  auth()->user()->createToken(
+            name: 'access token',
+            abilities: [TokenAbilityEnum::ACCESS_TOKEN->value],
+            expiresAt: $accessTokenExpiration
+        );
 
+        return response()->json([
+            'access_token' => $accessToken->plainTextToken,
+            'at_expired_at' => $accessTokenExpiration->format('Y-m-d H:i:s'),
+        ]);
+    }
+
+    /**
+     * Summary of logout
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function logout(): JsonResponse
+    {
+        auth()->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => "You're logout"
+        ]);
+    }
 }
--- routes/core/api_v1.php
+++ routes/core/api_v1.php
@@ -1,21 +1,25 @@
 <?php
 
+use App\Enums\TokenAbilityEnum;
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->middleware('guest:sanctum')->group(function(){
-    Route::post('login', [AuthController::class, 'login'])->name('core.login')->middleware('throttle:2,1');
-
+Route::prefix('auth')->middleware('guest:sanctum')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
 });
 
+Route::prefix('auth')->middleware(['auth:sanctum', 'ability:' . TokenAbilityEnum::ISSUE_ACCESS_TOKEN->value])->group(function () {
+    Route::post('refresh-token', [AuthController::class, 'refreshToken']);
+});
 
-
-Route::prefix('auth')->middleware('auth:sanctum')->group(function(){
-    Route::get('test2', function(){
-        return 'authorized';
+Route::prefix('auth')->middleware(['auth:sanctum', 'ability:' . TokenAbilityEnum::ACCESS_TOKEN->value])->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('test', function () {
+        return response()->json([
+            'message' => 'authenticated'
+        ]);
     });
 });
-
 
 Route::get('posts', [PostController::class, 'posts']);
